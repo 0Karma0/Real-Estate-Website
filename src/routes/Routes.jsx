5f9b9108906af001694a695a8f1ab89540a36e83
mahade <mahade.adib45@gmail.com>
2024-04-14 18:31:46 +0600

    added error page

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -3,11 +3,13 @@ import MainLayout from '../layout/mainLayout'
 import Home from '../Pages/Home'
 import Login from "../Pages/Login";
 import Register from "../Pages/Register";
+import ErrorPage from '../Pages/ErrorPage';
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <MainLayout />,
+    errorElement: <ErrorPage></ErrorPage>,
     children: [
       {
         path: '/',
